afaf4c046ddb59baadb237ba42daec988c03015e
ShyAnne <shyannetech1@gmail.com>
2025-08-27 02:38:08 +0100

    Done Done Done

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Maincontent from "../component/Maincontent";
+
 import Donation from "../component/Donation";
 import Footer from "../component/Footer";
 import MainPage from "../component/MainPage";
